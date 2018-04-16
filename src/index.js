0551736153395e7a451deaf08b0f4ce6fbb234f1
jdmedlock <jdmedlock@gmail.com>
2018-04-15 20:08:59 -0500

    feature: Refine layout
    Refine layout
    
    Resolves: N/a
    See also: N/a

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ function getReposAndEvents() {
   return gitaclue.get([
     { context: 'repo', contextOwner: 'chingu-voyage4', contextName: 'Bears-Team-0', segments: ['events']},
     { context: 'repo', contextOwner: 'chingu-voyage4', contextName: 'Bears-Team-1', segments: ['events']},
+    /*
     { context: 'repo', contextOwner: 'chingu-voyage4', contextName: 'Bears-Team-2', segments: ['events']},
     { context: 'repo', contextOwner: 'chingu-voyage4', contextName: 'Bears-Team-3', segments: ['events']},
     { context: 'repo', contextOwner: 'chingu-voyage4', contextName: 'Bears-Team-4', segments: ['events']},
@@ -46,6 +47,7 @@ function getReposAndEvents() {
     { context: 'repo', contextOwner: 'chingu-voyage4', contextName: 'Bears-Team-27', segments: ['events']},
     { context: 'repo', contextOwner: 'chingu-voyage4', contextName: 'Bears-Team-28', segments: ['events']},
     { context: 'repo', contextOwner: 'chingu-voyage4', contextName: 'Bears-Team-29', segments: ['events']},
+    */
   ]);
 }
 
